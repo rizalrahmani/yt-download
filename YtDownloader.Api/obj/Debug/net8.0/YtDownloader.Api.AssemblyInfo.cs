ab316bf30da7843d4f4918baf7cb5741099234d1
Mochamad Rizal Rahmani <mochrizalrah@gmail.com>
2026-07-09 17:17:02 +0700

    Update binaries and assembly info for YtDownloader.Api

--- YtDownloader.Api/obj/Debug/net8.0/YtDownloader.Api.AssemblyInfo.cs
+++ YtDownloader.Api/obj/Debug/net8.0/YtDownloader.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YtDownloader.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f51cdffb792468fb336aa332dc261a0f4e028c54")]
 [assembly: System.Reflection.AssemblyProductAttribute("YtDownloader.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YtDownloader.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
